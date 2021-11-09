11592155604f1052abc6464c67a14eaa35c21d80
GRPKosmick <94019231+GRPKosmick@users.noreply.github.com>
2021-11-09 16:11:17 -0600

    Little tweaks in the spanish translation

--- spanish.ts
+++ spanish.ts
@@ -3,15 +3,15 @@ import { Translations } from "..";
 export const SpanishTranslations: Translations = {
     WelcomeName: "Bienvenido/a a skeld.net",
     WelcomeMessage: `
-El primer servidor personalizado de Among Us del mundo
-Escribe /help para empezar
+Somos el primer servidor personalizado de Among Us del mundo
+Escribe /help sí necesitas ayuda
 Modo de juego actual: @{gamemode[0]}
 
-¡Suscríbete a nuestro Patreon https://www.patreon.com/skeld_net para tener un nombre de usuario colorido!`.trim(),
+¡Suscríbete a nuestro Patreon https://www.patreon.com/skeld_net para tener un nombre de usuario de colores!`.trim(),
     SetImpostorsInvalidCount:
-        "Número de jugadores inválido, por favor introduce un número entre 1 y 3",
+        "Número de impostores inválido, por favor introduzca un número entre 1 y 3",
     SetImpostorsSuccess: "El número de impostores es @{number[0]}",
-    ZombiesIncomingIn: "Los zombies llegan en @{number[0]} segundos",
+    ZombiesIncomingIn: "Los zombies aparecerán en @{number[0]} segundos",
     GamemodeChangedTo: "Modo de juego cambiado a @{gamemode[0]}",
     SquidGameLightsInstructions:
         "Pulsa el botón de reunión antes de que el tiempo se acabe",
@@ -21,5 +21,5 @@ Modo de juego actual: @{gamemode[0]}
         "Muévete en la dirección correcta para tirar del otro equipo",
     StartingIn: "Empezando en: ",
     MaxPlayersModeSwitch:
-        "El anfitrión cambió el modo y ahora esta sala está llena",
+        "El anfitrión cambió el número de jugadores y ahora la sala está llena",
 };
